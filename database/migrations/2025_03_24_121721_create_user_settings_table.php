b58509a2c83c3d645b26e6928becfe22bc3109b9
Twimm <hicli0001@gmail.com>
2025-03-24 13:32:26 +0100

    ...

--- database/migrations/2025_03_24_121721_create_user_settings_table.php
+++ database/migrations/2025_03_24_121721_create_user_settings_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->integer('due_date_delay')->nullable(); 
+            $table->integer('due_date_delay')->default(10)->nullable(); 
         });
     }
 
